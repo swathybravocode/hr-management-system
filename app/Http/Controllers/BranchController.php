72deaff9e3cab4f28863be9ef5beca27e0752815
jishnuprasadb <najiyanazzrin@gmail.com>
2022-03-17 15:55:05 +0530

    public function branchEmployeeNumber($branch_id, $type)

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -10,58 +10,48 @@ class BranchController extends Controller
 {
     public function index()
     {
-        if(\Auth::user()->can('Manage Branch'))
-        {
+        if (\Auth::user()->can('Manage Branch')) {
             $branches = Branch::where('created_by', '=', \Auth::user()->creatorId())->get();
 
             return view('branch.index', compact('branches'));
-        }
-        else
-        {
+        } else {
             return redirect()->back()->with('error', __('Permission denied.'));
         }
     }
 
     public function create()
     {
-        if(\Auth::user()->can('Create Branch'))
-        {
+        if (\Auth::user()->can('Create Branch')) {
             return view('branch.create');
-        }
-        else
-        {
+        } else {
             return response()->json(['error' => __('Permission denied.')], 401);
         }
     }
 
     public function store(Request $request)
     {
-        if(\Auth::user()->can('Create Branch'))
-        {
+        if (\Auth::user()->can('Create Branch')) {
 
             $validator = \Validator::make(
                 $request->all(), [
-                                   'name' => 'required',
-                                   'branch_name'=>'required',
-                               ]
+                    'name' => 'required',
+                    'branch_name' => 'required',
+                ]
             );
-            if($validator->fails())
-            {
+            if ($validator->fails()) {
                 $messages = $validator->getMessageBag();
 
                 return redirect()->back()->with('error', $messages->first());
             }
 
-            $branch             = new Branch();
-            $branch->name       = $request->name;
+            $branch = new Branch();
+            $branch->name = $request->name;
             $branch->branch_name = $request->branch_name;
             $branch->created_by = \Auth::user()->creatorId();
             $branch->save();
 
             return redirect()->route('branch.index')->with('success', __('Branch  successfully created.'));
-        }
-        else
-        {
+        } else {
             return redirect()->back()->with('error', __('Permission denied.'));
         }
     }
@@ -73,38 +63,29 @@ class BranchController extends Controller
 
     public function edit(Branch $branch)
     {
-        if(\Auth::user()->can('Edit Branch'))
-        {
-            if($branch->created_by == \Auth::user()->creatorId())
-            {
+        if (\Auth::user()->can('Edit Branch')) {
+            if ($branch->created_by == \Auth::user()->creatorId()) {
 
                 return view('branch.edit', compact('branch'));
-            }
-            else
-            {
+            } else {
                 return response()->json(['error' => __('Permission denied.')], 401);
             }
-        }
-        else
-        {
+        } else {
             return response()->json(['error' => __('Permission denied.')], 401);
         }
     }
 
     public function update(Request $request, Branch $branch)
     {
-        if(\Auth::user()->can('Edit Branch'))
-        {
-            if($branch->created_by == \Auth::user()->creatorId())
-            {
+        if (\Auth::user()->can('Edit Branch')) {
+            if ($branch->created_by == \Auth::user()->creatorId()) {
                 $validator = \Validator::make(
                     $request->all(), [
-                                       'name' => 'required',
-                                       'branch_name'=>'required',
-                                   ]
+                        'name' => 'required',
+                        'branch_name' => 'required',
+                    ]
                 );
-                if($validator->fails())
-                {
+                if ($validator->fails()) {
                     $messages = $validator->getMessageBag();
 
                     return redirect()->back()->with('error', $messages->first());
@@ -115,35 +96,25 @@ class BranchController extends Controller
                 $branch->save();
 
                 return redirect()->route('branch.index')->with('success', __('Branch successfully updated.'));
-            }
-            else
-            {
+            } else {
                 return redirect()->back()->with('error', __('Permission denied.'));
             }
-        }
-        else
-        {
+        } else {
             return redirect()->back()->with('error', __('Permission denied.'));
         }
     }
 
     public function destroy(Branch $branch)
     {
-        if(\Auth::user()->can('Delete Branch'))
-        {
-            if($branch->created_by == \Auth::user()->creatorId())
-            {
+        if (\Auth::user()->can('Delete Branch')) {
+            if ($branch->created_by == \Auth::user()->creatorId()) {
                 $branch->delete();
 
                 return redirect()->route('branch.index')->with('success', __('Branch successfully deleted.'));
-            }
-            else
-            {
+            } else {
                 return redirect()->back()->with('error', __('Permission denied.'));
             }
-        }
-        else
-        {
+        } else {
             return redirect()->back()->with('error', __('Permission denied.'));
         }
     }
@@ -151,12 +122,9 @@ class BranchController extends Controller
     public function getdepartment(Request $request)
     {
 
-        if($request->branch_id == 0)
-        {
+        if ($request->branch_id == 0) {
             $departments = Department::get()->pluck('name', 'id')->toArray();
-        }
-        else
-        {
+        } else {
             $departments = Department::where('branch_id', $request->branch_id)->get()->pluck('name', 'id')->toArray();
         }
 
@@ -165,12 +133,9 @@ class BranchController extends Controller
 
     public function getemployee(Request $request)
     {
-        if(in_array('0', $request->department_id))
-        {
+        if (in_array('0', $request->department_id)) {
             $employees = Employee::get()->pluck('name', 'id')->toArray();
-        }
-        else
-        {
+        } else {
             $employees = Employee::whereIn('department_id', $request->department_id)->get()->pluck('name', 'id')->toArray();
         }
 
@@ -181,72 +146,60 @@ class BranchController extends Controller
     {
         $branch_id = $request->branch_id;
 
-        $branch = Branch::where('id',$branch_id)->first();
+        $branch = Branch::where('id', $branch_id)->first();
 
-        $emp_number  = $this->branchEmployeeNumber($branch_id);
+        $emp_number = $this->branchEmployeeNumber($branch_id, 0);
+        $new_emp_number = $this->branchEmployeeNumber($branch_id, 1);
 
         $num_start_final = 0;
 
-        if($branch->name=='KER')
-        {
+        if ($branch->name == 'KER') {
             $num_start = 102;
-            $num_start_final = $num_start+$emp_number;
-            $branch_code = "IHC/".$branch->name."/".$num_start_final;
+            $num_start_final = $num_start + $emp_number;
+            $branch_code = "IHC/" . $branch->name . "/" . $num_start_final;
 
             $new_emp_number = $emp_number;
-            $new_emp_series = str_pad($new_emp_number,3,"0",STR_PAD_LEFT);
-            $new_emp_code   = "EY/".$branch->name."/".$new_emp_series;
+            $new_emp_series = str_pad($new_emp_number, 3, "0", STR_PAD_LEFT);
+            $new_emp_code = "EY/" . $branch->name . "/" . $new_emp_series;
 
-         }
-
-        // elseif($branch->name=='KR')
-        // {
-        //     $num_start = 102;
-        //     $num_start_final = $num_start+$emp_number;
-        //     $branch_code = "IHC/".$branch->name."/".$num_start_final;
-
-        // }
-
-        elseif($branch->name=='KNK')
-        {
+        } elseif ($branch->name == 'KNK') {
+            $emp_number = Employee::where('branch_id', '=', $branch_id)->orderBy('old_employee_code', 'DESC')->first();
+            $emp_number = explode('/', $emp_number->old_employee_code);
+            $emp_number = $emp_number[2] + 1;
             $val = $emp_number;
-            $num_start_a = str_pad($val,3,"0",STR_PAD_LEFT); // 0001
+            $num_start_a = str_pad($val, 3, "0", STR_PAD_LEFT); // 0001
             $num_start_final = $num_start_a;
+            $branch_code = "EY/KA/" . $num_start_final;
 
-            $branch_code = "EY/KA/".$num_start_final;
-
-            $new_emp_number = $emp_number;
-            $new_emp_series = str_pad($new_emp_number,3,"0",STR_PAD_LEFT);
-            $new_emp_code   = "EY/".$branch->name."/".$new_emp_series;
-        }
-
-        elseif($branch->name=='TN')
-        {
+            $new_emp_number = Employee::where('branch_id', '=', $branch_id)->orderBy('employee_code', 'DESC')->first();
+            $new_emp_number = explode('/', $new_emp_number->employee_code);
+            $new_emp_number = $new_emp_number[2]+1;
+            $new_emp_series = str_pad($new_emp_number, 3, "0", STR_PAD_LEFT);
+            $new_emp_code = "EY/" . $branch->name . "/" . $new_emp_series;
+        } elseif ($branch->name == 'TN') {
             $num_start = 100;
-            $num_start_final = $num_start+$emp_number;
-
-            $branch_code = "IHC/".$branch->name."/".$num_start_final;
+            $num_start_final = $num_start + $emp_number;
+            $branch_code = "IHC/" . $branch->name . "/" . $num_start_final;
 
             $new_emp_number = $emp_number;
-            $new_emp_series = str_pad($new_emp_number,3,"0",STR_PAD_LEFT);
-            $new_emp_code   = "EY/".$branch->name."/".$new_emp_series;
+            $new_emp_series = str_pad($new_emp_number, 3, "0", STR_PAD_LEFT);
+            $new_emp_code = "EY/" . $branch->name . "/" . $new_emp_series;
         }
 
         return response()->json(collect([
             'new_code' => $new_emp_code,
             'old_code' => $branch_code,
         ])->toJson()
-    );
+        );
 
     }
 
-    public function branchEmployeeNumber($branch_id)
+    public function branchEmployeeNumber($branch_id, $type)
     {
         $latest = Employee::where([['created_by', '=', \Auth::user()->creatorId()], ['branch_id','=', $branch_id]])->latest()->first();
 
-        $arr_emp = Employee::where('branch_id','=',$branch_id)->orderBy('employee_code','DESC')->first();
-        $arr_emp =explode('/',$arr_emp->employee_code);
-        $count = $arr_emp[2];
+        $arr_emp = Employee::where('branch_id','=', $branch_id)->get();
+        $count = count($arr_emp);
 
         if(!$latest)
         {
